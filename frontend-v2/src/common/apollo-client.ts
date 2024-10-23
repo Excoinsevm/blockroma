d0f79d33d8f0c394f1513f47fc32e0311f44a757
Excoinsevm <149539033+Excoinsevm@users.noreply.github.com>
2024-10-23 16:47:57 +0100

    Update apollo-client.ts

--- frontend-v2/src/common/apollo-client.ts
+++ frontend-v2/src/common/apollo-client.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 import fetch from "isomorphic-unfetch";
 
-const apiGatewayUrl = "https://blockroma.com/ethw/mainnet/api-gateway/";
+const apiGatewayUrl = "https://explorer.bit-rock.io/api/";
 
 export const apolloClient = new ApolloClient({
   ssrMode: false,
